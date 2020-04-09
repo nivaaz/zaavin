fc073a5b99dc73e175d36b75f7ee09148bb314aa
nivaaz <nksehmbhi@gmail.com>
2020-04-09 21:16:29 +1000

    library for fontawsome added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout/layout"
 import Welcome from "../components/Welcome/Welcome"
+import SocialLinks from "../components/social_links/SocialLinks"
 import SEO from "../components/seo"
+import { faFigma, faLinkedin,  faGithub,faInstagram, faMedium, faCodepen} from '@fortawesome/free-brands-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+library.add(fab, faFigma, faLinkedin,  faGithub, faMedium, faCodepen, faInstagram);
 
 const IndexPage = () => (
   <Layout>
@@ -12,6 +17,7 @@ const IndexPage = () => (
     <Link to="/Projects/"> Projects </Link>
     <Link to="/TrelloAPI/"> Trello Pop </Link>
     <Link to="/Canvamily/"> Canvamily </Link>
+    <SocialLinks/>
   </Layout>
 )
 
